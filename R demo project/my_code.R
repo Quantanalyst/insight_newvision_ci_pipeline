f6a79d4ff4049db412f8630e0e4b50c60a8cf32e
Saeed Mohajeryami,PhD <smohajer85@gmail.com>
2020-06-19 17:34:45 -0700

    documentation change

--- R demo project/my_code.R
+++
@@ -1,8 +0,0 @@
-# Example for simple function to be tested
-#
-# 1. Change working directory
-# 2. Call: testthat::test_dir('tests') within RStudio or R
-
-increment <- function(value) {
-  value + 1
-}
