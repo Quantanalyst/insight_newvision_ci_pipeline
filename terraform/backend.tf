92b4adddfe62c4c37cc4e30bd695c67b835dbebc
Saeed Mohajeryami,PhD <smohajer85@gmail.com>
2020-07-06 13:54:06 -0700

    documentation change

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "newvision_cicd_sm"
     key    = "terraform.tfstate"
     region = "us-west-2"
+    # integrate with dynamodb to support state locking
+    dynamodb_table = "s3-state-lock"
   }
 }
